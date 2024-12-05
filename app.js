a78d02b38ec336e001990d223e96467f63e8f7f3
zambaelinsano <lr663007@gmail.com>
2024-12-04 20:22:04 -0600

    first commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.use(express.static('Doctrack2')); // Sirve los archivos HTML, CSS, JS
+app.use(express.static('public')); // Sirve los archivos HTML, CSS, JS
 
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto 3000 :)`);
